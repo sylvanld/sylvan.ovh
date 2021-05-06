27411c14ceb615e4dccedb1be5d681a72afc155c
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2021-05-06 17:55:27 +0200

    change layout theme

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,23 +1,39 @@
 <template>
   <v-container>
-    <img
-      src="https://media-exp1.licdn.com/dms/image/C4D03AQEv5pwuqH27wA/profile-displayphoto-shrink_100_100/0/1592516435483?e=1625702400&v=beta&t=Blk4ad1rNtLA2pDgeCqWQJO-afDqtJ16Xsghl_RVeUE"
-      alt="photo-of-me-(lol)"
-      style="border-radius: 50%"
-    />
-    <h1>Hi there!</h1>
-    <h2>
-      I am <a href="https://github.com/sylvanld">@hoshiyosan</a> (Sylvan Le
-      Deunff)
-    </h2>
-    <p>
-      I currently work as a software engineer at Orange Business Services (in
-      France).
-    </p>
+    <v-row style="align-items: center">
+      <v-col cols="auto" md="8">
+        <img
+          src="https://media-exp1.licdn.com/dms/image/C4D03AQEv5pwuqH27wA/profile-displayphoto-shrink_100_100/0/1592516435483?e=1625702400&v=beta&t=Blk4ad1rNtLA2pDgeCqWQJO-afDqtJ16Xsghl_RVeUE"
+          alt="photo-of-me-(lol)"
+          style="border-radius: 50%"
+        />
+        <h1>Hi there!</h1>
+        <h2>
+          I am <a href="https://github.com/sylvanld">@hoshiyosan</a> (Sylvan Le
+          Deunff)
+        </h2>
+        <p>
+          I currently work as a software engineer at Orange Business Services
+          (in France).
+        </p>
 
-    <p>
-      In my spare time, I like to develop many side projects, to improve my
-      skills and learn new things, but mostly for fun!
-    </p>
+        <p>
+          In my spare time, I like to develop many side projects, to improve my
+          skills and learn new things, but mostly for fun!
+        </p>
+      </v-col>
+      <v-col cols="auto" md="4">
+        <figure>
+          <img
+            src="@/assets/raspberry.jpg"
+            alt="home-made kubernetes cluster with 3 raspberry pi"
+          />
+          <figcaption>
+            Home-made kubernetes cluster of 3 raspberry pi <br />
+            this site is hosted on.
+          </figcaption>
+        </figure>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
