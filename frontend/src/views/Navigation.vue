148683546cb4b8c89677f183add4f18f4c91a70e
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2021-05-09 00:31:46 +0200

    add navigation entry for blog

--- frontend/src/views/Navigation.vue
+++ frontend/src/views/Navigation.vue
@@ -45,6 +45,10 @@
           <v-icon>mdi-school</v-icon>
           Curriculum
         </v-tab>
+        <v-tab :to="{ name: 'Blog' }">
+          <v-icon>mdi-file-document-edit</v-icon>
+          Blog
+        </v-tab>
       </v-tabs>
     </template>
   </v-app-bar>
