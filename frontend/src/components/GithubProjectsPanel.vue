e01e6d46ef377345a9f714375132b77c1b152b4e
Sylvan Le Deunff <sylvan.ledeunff@orange.com>
2021-05-08 16:39:39 +0200

    add search in projects panel

--- frontend/src/components/GithubProjectsPanel.vue
+++ frontend/src/components/GithubProjectsPanel.vue
@@ -1,12 +1,16 @@
 <template>
   <div>
     <form action="">
-      <v-text-field label="Browse projects..." append-icon="mdi-magnify" />
+      <v-text-field
+        label="Browse projects..."
+        v-model="search"
+        append-icon="mdi-magnify"
+      />
     </form>
 
     <div class="panel">
       <github-project-card
-        v-for="(project, index) in showcasedRepositories"
+        v-for="(project, index) in filteredProjects"
         class="panel-card"
         :key="index"
         :project="project"
@@ -19,14 +23,55 @@
 import { mapGetters } from "vuex";
 import GithubProjectCard from "@/components/GithubProjectCard";
 
+function computePertinence(project, search) {
+  const projectName = project.name.toLowerCase();
+  const projectDescription = (project.description || "").toLowerCase();
+  const projectTopics = project.topics.join(" ").toLowerCase();
+  const searchTerms = search.toLowerCase().split(" ");
+
+  // try to find them in project
+  let score = 0;
+  for (let term of searchTerms) {
+    if (projectName.includes(term)) {
+      score += 3;
+    }
+    if (projectDescription.includes(term)) {
+      score += 1;
+    }
+    if (projectTopics.includes(term)) {
+      score += 2;
+    }
+  }
+  return score;
+}
+
 export default {
   components: { GithubProjectCard },
   data() {
     return {
-      projects: [],
+      search: null,
     };
   },
   computed: {
+    filteredProjects() {
+      if (!this.showcasedRepositories) {
+        return [];
+      }
+
+      if (this.search) {
+        // if search is provided, apply search algorithm to filter/sort results
+        const projectsPertinences = this.showcasedRepositories.map(
+          (project) => [computePertinence(project, this.search), project]
+        );
+        return projectsPertinences
+          .filter((pp) => pp[0] > 0)
+          .sort((ppA, ppB) => ppA[0] < ppB[0])
+          .map((pp) => pp[1]);
+      } else {
+        // if no search is provided, take most pertinent project based on their content
+        return this.showcasedRepositories;
+      }
+    },
     ...mapGetters("github", ["showcasedRepositories"]),
   },
   mounted() {
